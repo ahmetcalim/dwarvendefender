41d1720ea6d2c5ecafc59e2b03d1f7ec0d790ca6
ahmetcalim <35461154+ahmetcalim@users.noreply.github.com>
2019-11-18 12:51:16 +0300

    Eski AI
    Eski AI silindi

--- Assets/DEV/Scripts/AI/Framework/AIAction.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class AIAction : ScriptableObject {
-
-    public AICondition[] Conditions;
-    public abstract void Act(AIStateMachine fsm);
-}
